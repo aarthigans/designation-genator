9c9dd6f67b94d9c19f76c7692ed2b33ded723cd6
Lakshmi <mlakshmi.76@gmail.com>
2022-07-14 01:35:58 -0400

    tests failed-assertion fixed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,21 +3,21 @@ const Engineer = require('../lib/Engineer');
 
 // creating engineer object  
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Name2', 90, 'name2@gmail.com');
+    const engineer = new Engineer('Name2', 90, 'name2@gmail.com', 'https://github.com/name2');
     
     expect(engineer.github) .toEqual(expect.any(String));
 });
 
 // gets github from getGithub()
 test('gets engineer github value', () => {
-    const engineer = new Engineer('Name2', 90, 'name2@gmail.com');
+    const engineer = new Engineer('Name2', 90, 'name2@gmail.com', 'https://github.com/name2');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 // gets role from getRole() 
 test('gets role of employee', () => {
-    const engineer = new Engineer('Name2', 90, 'name2@gmail.com');
+    const engineer = new Engineer('Name2', 90, 'name2@gmail.com', 'https://github.com/name2');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
